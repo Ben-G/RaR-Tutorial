42fb4abe91009b05d7939491ccbeedf44dd1ffa4
Ben-G <Benjamin.Encz@gmail.com>
2013-03-03 16:54:39 +0100

    completed excersis 3.5.1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -44,4 +44,18 @@ describe "Static pages" do
     end
   end
 
+  describe "Contact page" do 
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+        :text => "| Contact")
+    end
+  end
+
 end
