36047c7e52dfc9f50c900453cc41307755b24513
Ben-G <Benjamin.Encz@gmail.com>
2013-04-02 22:09:04 +0200

    Finish layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   root to: 'static_pages#home'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/signup',  to: 'users#new'
 
   get 'static_pages/about'
   get 'static_pages/help'
